c61ccb7adb67f5c15f6729a65630842ac35f48ad
kewablay <kojokewajunior123@gmail.com>
2024-09-24 15:34:05 +0000

    added pages and initial route setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/media-list/media-list.component').then(
+        (m) => m.MediaListComponent
+      ),
+  },
+  {
+    path: 'sign-in',
+    loadComponent: () =>
+      import('./pages/sign-in/sign-in.component').then(
+        (m) => m.SignInComponent
+      ),
+  },
+  {
+    path: 'sign-up',
+    loadComponent: () =>
+      import('./pages/sign-up/sign-up.component').then(
+        (m) => m.SignUpComponent
+      ),
+  },
+];
