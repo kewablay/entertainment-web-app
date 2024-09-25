90714eb668316a17a9a993803c0bb33df22cf62a
kewablay <kojokewajunior123@gmail.com>
2024-09-25 13:55:54 +0000

    added app model

---
+++ src/app/core/models/app.model.ts
@@ -0,0 +1,21 @@
+export interface Thumbnail {
+  trending: {
+    small: string;
+    large: string;
+  };
+  regular: {
+    small: string;
+    medium: string;
+    large: string;
+  };
+}
+
+export interface Media {
+  title: string;
+  thumbnail: Thumbnail;
+  year: number;
+  category: string;
+  rating: string;
+  isBookmarked: boolean;
+  isTrending: boolean;
+}
