9c39b350c4f8ad794104ce01485d1f6b948d1c82
kewablay <kojokewajunior123@gmail.com>
2024-09-25 23:35:33 +0000

    fixed routing issue and cleaned up ui

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: '',
+    path: 'home',
     loadComponent: () =>
       import('./features/home/home.component').then((m) => m.HomeComponent),
   },
