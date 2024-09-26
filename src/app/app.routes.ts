53a5cb4b7fb377f528a8b1d7e266511174d56e5c
kewablay <kojokewajunior123@gmail.com>
2024-09-26 17:12:10 +0000

    added route layouts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,27 +1,85 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+  // AUTH ROUTES
   {
-    path: 'home',
+    path: '',
     loadComponent: () =>
-      import('./features/home/home.component').then((m) => m.HomeComponent),
-  },
-  {
-    path: 'media/:category',
-    loadComponent: () =>
-      import('./features/media-category/media-category.component').then(
-        (m) => m.MediaCategoryComponent
+      import('./core/layouts/auth-layout/auth-layout.component').then(
+        (m) => m.AuthLayoutComponent
       ),
+    children: [
+      {
+        path: 'signup',
+        loadComponent: () =>
+          import('./features/sign-up/sign-up.component').then(
+            (m) => m.SignUpComponent
+          ),
+      },
+      {
+        path: 'login',
+        loadComponent: () =>
+          import('./features/sign-in/sign-in.component').then(
+            (m) => m.SignInComponent
+          ),
+      },
+    ],
   },
+
+  // MAIN APPLICATION ROUTES
   {
-    path: 'bookmarks',
+    path: '',
+    // canActivate: [AuthGuard],
     loadComponent: () =>
-      import('./features/bookmarks/bookmarks.component').then(
-        (m) => m.BookmarksComponent
+      import('./core/layouts/main-layout/main-layout.component').then(
+        (m) => m.MainLayoutComponent
       ),
+    children: [
+      {
+        path: 'home',
+        loadComponent: () =>
+          import('./features/home/home.component').then((m) => m.HomeComponent),
+      },
+      {
+        path: 'media/:category',
+        loadComponent: () =>
+          import('./features/media-category/media-category.component').then(
+            (m) => m.MediaCategoryComponent
+          ),
+      },
+      {
+        path: 'bookmarks',
+        loadComponent: () =>
+          import('./features/bookmarks/bookmarks.component').then(
+            (m) => m.BookmarksComponent
+          ),
+      },
+    ],
   },
-  {
-    path: '**',
-    redirectTo: 'home',
-  },
+  // Default Route
+  { path: '**', redirectTo: '/home', pathMatch: 'full' },
 ];
+
+// {
+//   path: 'home',
+//   loadComponent: () =>
+//     import('./features/home/home.component').then((m) => m.HomeComponent),
+// },
+// {
+//   path: 'media/:category',
+//   loadComponent: () =>
+//     import('./features/media-category/media-category.component').then(
+//       (m) => m.MediaCategoryComponent
+//     ),
+// },
+// {
+//   path: 'bookmarks',
+//   loadComponent: () =>
+//     import('./features/bookmarks/bookmarks.component').then(
+//       (m) => m.BookmarksComponent
+//     ),
+// },
+// {
+//   path: '**',
+//   redirectTo: 'home',
+// },
