7857be4ca036ef734551ad74e6015e77bb2df1a1
kewablay <kojokewajunior123@gmail.com>
2024-09-26 11:41:39 +0000

    added bookmark media implementation

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Media } from '../../../core/models/app.model';
+import { Store } from '@ngrx/store';
+import { bookmarkMedia } from '../../../core/store/media/media.actions';
 
 @Component({
   selector: 'app-card',
@@ -11,4 +13,11 @@ import { Media } from '../../../core/models/app.model';
 export class CardComponent {
   @Input() media!: Media;
   @Input() isTrending!: boolean;
+
+  constructor(private store: Store) {}
+
+
+  onBookmarkClick() {
+    this.store.dispatch(bookmarkMedia({ mediaId: this.media.id }));
+  }
 }
