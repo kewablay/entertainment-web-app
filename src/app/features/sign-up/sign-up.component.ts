67574d2b5f1adcf8ef6d05ef4a1fbaf6f2b97413
kewablay <kojokewajunior123@gmail.com>
2024-09-27 10:46:08 +0000

    added signup form with validators and error handling

--- src/app/features/sign-up/sign-up.component.ts
+++ src/app/features/sign-up/sign-up.component.ts
@@ -1,12 +1,60 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { AuthService } from '../../core/services/auth-service/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './sign-up.component.html',
-  styleUrl: './sign-up.component.sass'
+  styleUrl: './sign-up.component.sass',
 })
 export class SignUpComponent {
+  signUpForm!: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.signUpForm = this.fb.group(
+      {
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        confirmPassword: ['', [Validators.required]],
+      },
+      { validator: this.passwordMatchValidator }
+    );
+  }
+
+  // Custom validator to check if passwords match
+  passwordMatchValidator(form: FormGroup) {
+    const password = form.get('password')?.value;
+    const confirmPassword = form.get('confirmPassword')?.value;
+    return password === confirmPassword ? null : { mismatch: true };
+  }
+
+  onSubmit() {
+    if (this.signUpForm.valid) {
+      const { email, password } = this.signUpForm.value;
+      const signUpData = { email, password };
+
+      this.authService.signUp(signUpData).subscribe((response) => {
+        console.log('response from signup request: ', response);
+        this.router.navigate(['/login']);
+      });
+    } else {
+      console.log('signup form invalid');
+      this.signUpForm.markAllAsTouched();
+    }
+  }
 }
